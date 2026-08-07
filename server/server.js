cef2be88b87953a71f53e93b6300e933543ba870
Pham Chiesa <Admin@Huster.(none)>
2026-08-07 15:11:03 +0700

    fix vervel error

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import {serve} from "inngest/express"
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 await connectDB();
 
@@ -18,14 +18,16 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
-
 // API Routes
 app.get("/", (req, res) => 
     res.send("Server is live")
 );
 app.use('/api/inngest', serve({client: inngest, functions}));
 
+if (!process.env.VERCEL) {
+    app.listen(port, () => {
+        console.log(`Server is running on PORT ${port}`);
+    });
+}
 
-app.listen(port, () => {
-    console.log(`Server is running on PORT ${port}`);
-})
+export default app;
