1f8c83cf3b6683773253ab28caab2848823cc194
Pham Chiesa <Admin@Huster.(none)>
2026-08-07 10:52:23 +0700

    fix trigger error

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
 // Thay vì code CRUD truyền thống thì mk dùng thư viện clerk và inngest
-import { User } from "../models/User.js";
+import User from "../models/User.js";
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
@@ -7,8 +7,10 @@ export const inngest = new Inngest({id: 'movie-ticket-booking'});
 
 // Inngest Function to save user data to a db
 const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created'},
+    {
+        id: 'sync-user-from-clerk',
+        triggers: { event: 'clerk/user.created' }
+    },
     async({event}) => {
         const {id, first_name, last_name, email_address, image_url} = event.data;
         const userData = {
@@ -23,8 +25,10 @@ const syncUserCreation = inngest.createFunction(
 
 // Inngest () to delete user from db
 const syncUserDeletion = inngest.createFunction(
-    {id: 'delete-user-with-clerk'},
-    {event: 'clerk/user.deleted'},
+    {
+        id: 'delete-user-with-clerk',
+        triggers: { event: 'clerk/user.deleted' }
+    },
     async({event}) => {
         const {id} = event.data;
         await User.findByIdAndDelete(id);
@@ -33,8 +37,10 @@ const syncUserDeletion = inngest.createFunction(
 
 // Inngest () to update user from db
 const syncUserUpdation = inngest.createFunction(
-    {id: 'update-user-from-clerk'},
-    {event: 'clerk/user.updated'},
+    {
+        id: 'update-user-from-clerk',
+        triggers: { event: 'clerk/user.updated' }
+    },
     async({event}) => {
         const {id, first_name, last_name, email_address, image_url} = event.data;
          const userData = {
